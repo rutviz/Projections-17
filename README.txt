62ba1352cac83a4627b254f9ad7f5bbaa34d5964
Rutviz Vyas <Rutviz Vyas>
2016-12-28 09:07:52 +0530

    home page updated

---
+++ README.txt
@@ -0,0 +1,10 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/mattsince87/pen/vdEbg.
+
+ This is an Animated (**responsive**) SVG Path with a Jquery Callback all using the awesome [SnapSVG][1] library! I've been playing with this library for about a month and this is what i've managed to come up with so far.
+
+* [SnapSVG][1]
+* Rebound of my other [Pen][2]
+* **Heart if you like!**
+
+  [1]: http://snapsvg.io/
+  [2]: http://codepen.io/mattsince87/pen/snqLy
--- index.php
+++ index.php
@@ -12,20 +12,23 @@
    
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	  <link rel="stylesheet" href="style.css"></link>
+	  <link rel="stylesheet" href="css/style.css"></link>
       <!--Import materialize.css-->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
-      <!--Let browser know website is optimized for mobile-->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></style>
+	
+	  <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	  
     </head>
 
-    <body class="black">
+    <body class="black" style="	overflow:hidden;">
+	
 	<?php
 	include("header.html");	
 	?>
 	<div class="shade">
-     
+    <div class="box1"></div>
 
       <article class="layer" >
         <div class="layer__background">
@@ -40,14 +43,27 @@
 		<div class="scene scene-05"></div>
         </div>
       </article>
-	  
-	  </div>	
+	  </div>
+	   <div id="total" class="animated hide-on-med-and-down" style="/*background-color:#336699;*/display:inline-block;top:35%;left:28%;position:absolute;">
+	<img class="animated bounceInDown" id="p3" src="png/3.png" width="18%"></img>
+	<img class="animated bounceInUp" id="p2" src="png/2.png" width="41%" ></img>
+	<img class="animated bounceInRight" id="p1" src="png/1.png" width="35%"></img>
+	<h1 class="animated flipInY" id="name">PROJECTIONS</h1>
+	</div>
+	   
+	 
+	
+	
+	 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-throttle-debounce/1.1/jquery.ba-throttle-debounce.js"></script>
-    <script src="top.js"></script>
+    <script src="js/top.js"></script>
     
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
 	<script>
+	$('#name').on('webkitAnimationEnd animationend',function(event){
+		$("#total").addClass('pulse');
+	} );
 	$( document ).ready(function(){
 		$(".button-collapse").sideNav();
 	});
