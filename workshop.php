92d3a22c712927118daa2cf1e8191923ac00ce7b
Rutviz Vyas <Rutviz Vyas>
2017-01-24 23:47:59 +0530

    workshop page done

--- workshop.php
+++ workshop.php
@@ -10,12 +10,13 @@
  <?php
 	include("header.html");	
 	?>
-	<div class="parallax-container">
-      <div class="parallax"><img src="http://materializecss.com/images/parallax1.jpg"></div>
+	<div class="parallax-container" style="height: 400px">
+      <div class="parallax"><img src="http://www.planwallpaper.com/static/images/City_Landscape_Background.jpg"></div>
     </div>
 	<div style="background-color:#e0e0e0">
 <div class="event-card">
-<div class="event-card_details">
+<div class="event-card_details" >
+<img src="logo/w1.png"/>
 </div>
  <div class="event-card__image__date">
       <i class="material-icons icon">&#xE192;</i>
@@ -33,7 +34,8 @@
   </div>
 </div>
 <div class="event-card">
-<div class="event-card_details">
+<div class="event-card_details" >
+<img src="logo/w2.png"/>
 </div>
  <div class="event-card__image__date">
       <i class="material-icons icon">&#xE192;</i>
@@ -49,6 +51,85 @@
     <button>About Workshop</button>
   </div>
 </div>
+</div>
+<div class="event-card">
+<div class="event-card_details" >
+<img src="logo/w3.png"/>
+</div>
+ <div class="event-card__image__date">
+      <i class="material-icons icon">&#xE192;</i>
+      <p><span class="number">11</span> JUN</p>
+    </div>
+	<div class="event-card__image__notif">  Registration Fee: Rs 500 per Individual</div>
+
+<div class="event-card__text">
+    <h1>Hacktrack</h1>
+    <p>For the enthusiastic beginners to learn the basics of Ethical hacking</p>
+	<h3>Speaker: Sunny Wagela</h3>
+	
+    <button>About Workshop</button>
+  </div>
+</div>
+</div>
+<div class="event-card">
+<div class="event-card_details" >
+<img src="logo/w4.png"/>
+</div>
+ <div class="event-card__image__date">
+      <i class="material-icons icon">&#xE192;</i>
+      <p><span class="number">11</span> JUN</p>
+    </div>
+	<div class="event-card__image__notif">  Registration Fee: Rs 500 per Individual</div>
+
+<div class="event-card__text">
+    <h1>Hacktrack</h1>
+    <p>For the enthusiastic beginners to learn the basics of Ethical hacking</p>
+	<h3>Speaker: Sunny Wagela</h3>
+	
+    <button>About Workshop</button>
+  </div>
+</div>
+</div>
+<div class="event-card">
+<div class="event-card_details" >
+<img src="logo/w5.png"/>
+</div>
+ <div class="event-card__image__date">
+      <i class="material-icons icon">&#xE192;</i>
+      <p><span class="number">11</span> JUN</p>
+    </div>
+	<div class="event-card__image__notif">  Registration Fee: Rs 500 per Individual</div>
+
+<div class="event-card__text">
+    <h1>Hacktrack</h1>
+    <p>For the enthusiastic beginners to learn the basics of Ethical hacking</p>
+	<h3>Speaker: Sunny Wagela</h3>
+	
+    <button>About Workshop</button>
+  </div>
+</div>
+</div>
+<div class="event-card">
+<div class="event-card_details" >
+<img src="logo/w6.png"/>
+</div>
+ <div class="event-card__image__date">
+      <i class="material-icons icon">&#xE192;</i>
+      <p><span class="number">11</span> JUN</p>
+    </div>
+	<div class="event-card__image__notif">  Registration Fee: Rs 500 per Individual</div>
+
+<div class="event-card__text">
+    <h1>Hacktrack</h1>
+    <p>For the enthusiastic beginners to learn the basics of Ethical hacking</p>
+	<h3>Speaker: Sunny Wagela</h3>
+	
+    <button>About Workshop</button>
+  </div>
+</div>
+</div>
+<div style="height:500px; display:relative">
+hello
 </div>
 <script src="js/jquery-2.1.1.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
@@ -58,14 +139,16 @@
       $('.parallax').parallax();
 	  $(".button-collapse").sideNav();
     });
+
+			
  function navcolor(){
- 	 if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+ 	 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         $("header").css("background-color","#000");
     } else {
         $("header").css("background-color","transparent");
     }
  	
  }
-</script>
+	</script>
 </body>
 </html>
